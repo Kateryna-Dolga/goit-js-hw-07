ffa8dd6ce065c99cf3b28f809308313206975cae
Kateryna D <july_info@ukr.net>
2024-06-29 16:06:13 +0300

    commit

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+console.log("--------------------- task - 3--------------------------------");
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', function() {
+    const trimmedValue = nameInput.value.trim();
+    nameOutput.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
+});
